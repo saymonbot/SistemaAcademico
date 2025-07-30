ea43a23c9caa2c30b2aca16d62fae7c516b94cac
Saymon Botelho <82913820+saymonbot@users.noreply.github.com>
2025-07-30 18:11:15 -0300

    Update JanelaPopUpController.java

--- src/view/JanelaPopUpController.java
+++ src/view/JanelaPopUpController.java
@@ -9,14 +9,14 @@ public class JanelaPopUpController {
 	@FXML
 	private Label label2;
 	
-    @FXML
-    void fecharJanela() {
+	@FXML
+	void fecharJanela() {
     	
-    	try {
-    		Stage stage = (Stage) label2.getScene().getWindow();
-    		stage.close();
-    	} catch (Exception e) {
-    		e.printStackTrace();
-    	}
+	    	try {
+	    		Stage stage = (Stage) label2.getScene().getWindow();
+	    		stage.close();
+	    	} catch (Exception e) {
+	    		e.printStackTrace();
+	    	}
     }
 }
